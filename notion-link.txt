39a6e86cf0a318c105ccc3e7e78f0f1a00b59d8f
tnicacio <nicacio.t92@gmail.com>
2021-04-03 16:37:54 -0300

    Header and Home view

---
+++ notion-link.txt
@@ -0,0 +1,1 @@
+https://www.notion.so/Desafio-01-Criando-um-projeto-do-zero-b1a3645d286b4eec93f5f1f5476d0ff7
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,9 +26,63 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <main className={commonStyles.container}>
+      <div className={`${commonStyles.commonSubcontainer} ${styles.posts}`}>
+        <Link href="/">
+          <a>
+            <strong>Como utilizar Hooks e tal bla ble bli blo</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida</p>
+            <div className={styles.info}>
+              <time>
+                <FiCalendar /> 15 mar 2021
+              </time>
+              <span>
+                <FiUser /> Joseph Oliveira
+              </span>
+            </div>
+          </a>
+        </Link>
+        <Link href="/">
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam
+              modi molestiae minus temporibus amet eius ducimus eveniet
+              consectetur aliquam officia. Pensando em sincronização em vez de
+              ciclos de vida
+            </p>
+            <div className={styles.info}>
+              <time>
+                <FiCalendar /> 15 mar 2021
+              </time>
+              <span>
+                <FiUser /> Joseph Oliveira
+              </span>
+            </div>
+          </a>
+        </Link>
+        <Link href="/">
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida</p>
+            <div className={styles.info}>
+              <time>
+                <FiCalendar /> 15 mar 2021
+              </time>
+              <span>
+                <FiUser /> Joseph Oliveira
+              </span>
+            </div>
+          </a>
+        </Link>
+
+        <button type="button">Carregar mais posts</button>
+      </div>
+    </main>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
